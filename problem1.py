4c99c6194c9f6790e01acc03072cf835a8745a21
sangeetha <sangeethasrumail.com>
2025-03-11 19:01:46 +0000

    1problem

---
+++ problem1.py
@@ -0,0 +1,10 @@
+number1 = int(input("enter number "))
+number2 = int(input("enter number2"))
+
+if (number1*number2) >= 1000:
+    print(number1+number2)
+else:
+    print(number1*number2)
+    
+    
+
