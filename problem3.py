489203cd3c1ebc053e7d13fee1a4364c727b9eb5
sangeetha <sangeethasrumail.com>
2025-03-12 12:47:08 +0000

    even char

---
+++ problem3.py
@@ -0,0 +1,22 @@
+
+
+string = input("original string is: ")
+print("printing only even index characters")
+for i in range(len(string)):
+    if i%2 == 0:
+        print(string[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
