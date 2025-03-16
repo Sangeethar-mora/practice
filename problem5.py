a14b3470def4a191d8a5fa9663acdde000757b37
sangeetha <sangeethasrumail.com>
2025-03-16 16:57:45 +0000

    compare list

---
+++ problem5.py
@@ -0,0 +1,18 @@
+numbers_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
+
+first = numbers_x[0]
+last = numbers_x[-1]
+
+if first == last:
+    print("true",numbers_x)
+else:
+    print("false",numbers_x)
+
+first = numbers_y[0]
+last = numbers_y[-1]
+if first == last:
+    print("true",numbers_y)
+else:
+    print("false",numbers_y)
+
