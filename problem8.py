28ab0b32b1e85df1a19ef09a5354f96da9a1733c
sangeetha <sangeethasrumail.com>
2025-03-17 08:33:36 +0000

    palindrome

---
+++ problem8.py
@@ -0,0 +1,7 @@
+# original number 121
+num = list(input("original number is "))
+
+if num[0] == num[-1]:
+    print("Yes. given number is palindrome number")
+else:
+    print("no. given number is not a palindrome number")
