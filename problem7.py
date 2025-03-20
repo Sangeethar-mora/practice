87c279e763539e3ee0f347e92c7d29ad60077223
sangeetha <sangeethasrumail.com>
2025-03-20 11:10:48 +0000

    updated count of chars

--- problem7.py
+++ problem7.py
@@ -1,11 +1,55 @@
+# Exercise 7: Find the number of occurrences of a substring in a string
+# Write a Python code to find how often the substring “Emma” appears in the given string.
+#
+# Given:
+#
+# str_x = "Emma is good developer. Emma is a writer"
+# Expected Output:
+#
+# Emma appeared 2 times
 str_x = "Emma is good developer. Emma is a writer"
-str_lis = []
-wrd = ""
-for i in str_x:
+
+str_list = []
+
+wrd = " "
+
+for i  in str_x:
+    if i != " ":
+        wrd += i
     if i == " ":
-        str_lis.append(wrd)
-        wrd = ""
-    if i != ".":
-        wrd = wrd+i
-    str_lis.append(wrd)
-     print(str_lis)
+        str_list.append(wrd)
+        wrd = " "
+str_list.append(wrd)
+
+# print(str_list)
+
+unq_val = []
+
+for i in str_list:
+    if i not in unq_val:
+        unq_val.append(i)
+
+# print(unq_val)
+cnts = []
+for i in unq_val:
+    c = 0
+    for j in str_list:
+        if i == j:
+            c = c+1
+    cnts.append(c)
+    # print(i, c)
+# print(cnts)
+
+max_cnt = 0
+
+max_index = []
+
+for i in cnts:
+    if i is max(cnts):
+        max_index.append(max_cnt)
+    max_cnt += 1
+
+# print(max_index)
+
+for i in max_index:
+    print("word ", unq_val[i], "appeared", cnts[i], "times")
