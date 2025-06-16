a878b682de43ef5fb627a905823053b491d6dd6a
sangeetha <sangeethasrumail.com>
2025-06-16 18:42:03 +0100

    table

--- problem13.py
+++ problem13.py
@@ -1,5 +1,10 @@
-for i in range(1, 11):
-    for j in range(1, 11):
-        print(i * j, end=" ")
-    print()
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(i * j, end=" ")
+#     print()
+
+for i in range(1, 101):
+    print(i,end = " ")
+    if i % 10 == 0:
+        print()
 
