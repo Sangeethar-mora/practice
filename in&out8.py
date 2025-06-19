4dd0ac6acc398ab24a7ef3e0bb8ffd8e4dea1fa1
sangeetha <sangeethasrumail.com>
2025-06-19 16:04:43 +0100

    in&out

--- in&out8.py
+++ in&out8.py
@@ -3,5 +3,5 @@ totalMoney = 1000
 quantity = 3
 price = 450.00
 
-result = "I have {0} dollars so I can buy {1} football for {2} dollars.".format(totalMoney, quantity, price)
-print(result)
+print(f"I have {totalMoney} dollars so I can buy {quantity} football for {price} dollars.")
+
