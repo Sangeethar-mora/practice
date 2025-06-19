f5ceab20dabbabdb2a4be2f6bb923bfa0e440415
sangeetha <sangeethasrumail.com>
2025-06-19 16:13:40 +0100

    input and output

--- in&out7.py
+++ in&out7.py
@@ -1,8 +1,5 @@
-Name1 = input("Enter the first string: ")
-Name2 = input("Enter the second string: ")
-Name3 = input("Enter the third string: ")
+Name1, Name2, Name3 = input("enter three strings: ").split()
 
 print("Name1:", Name1)
 print("Name2:", Name2)
 print("Name3:", Name3)
-
